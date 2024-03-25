765c220655db04d4f20ef2801a4984425828c580
Yeji Kim <yejif15@gmail.com>
2024-03-25 17:55:50 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,44 +1,76 @@
-## VPC, Security group test
-resource "aws_vpc" "vpc-test-stn" {
-  cidr_block  = "10.0.0.0/16"
+resource "random_id" "example" {
+  byte_length = 8
+}
 
-  tags = {
-    Name = "vpc-test-stn"
+resource "aws_cloudtrail" "example" {
+  depends_on = [aws_s3_bucket_policy.example]
+
+  name                          = "example"
+  s3_bucket_name                = aws_s3_bucket.example.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
+  is_multi_region_trail         = false
+  enable_log_file_validation = true
+
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket-${random_id.example.hex}"
+  force_destroy = true
+}
+
+data "aws_iam_policy_document" "example" {
+  statement {
+    sid    = "AWSCloudTrailAclCheck"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.example.arn]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
+    }
+  }
+
+  statement {
+    sid    = "AWSCloudTrailWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.example.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
+    }
   }
 }
 
-resource "aws_security_group" "poc-workspace_sg" {
-  name        = "poc-workspace_sg"
-  vpc_id = aws_vpc.vpc-test-stn.id
-  description = "Security Group poc-workspace-sg"
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.example.json
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22  # SSH
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  # 세 번째 ingress 규칙
-  ingress {
-    from_port   = 443  # HTTPS
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "poc-workspace"
-  }
 }
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_region" "current" {}
