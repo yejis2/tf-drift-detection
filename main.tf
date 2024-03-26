939b92a2077b3a433552e3beb67c201f7a74ec10
Yeji Kim <yejif15@gmail.com>
2024-03-26 10:39:24 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@ resource "random_id" "example" {
   byte_length = 8
 }
 
-resource "aws_cloudtrail" "example" {
-  depends_on = [aws_s3_bucket_policy.example]
+# resource "aws_cloudtrail" "example" {
+#   depends_on = [aws_s3_bucket_policy.example]
 
-  name                          = "example"
-  s3_bucket_name                = aws_s3_bucket.example.id
-  s3_key_prefix                 = "prefix"
-  include_global_service_events = false
-  is_multi_region_trail         = false
-  enable_log_file_validation = true
+#   name                          = "example"
+#   s3_bucket_name                = aws_s3_bucket.example.id
+#   s3_key_prefix                 = "prefix"
+#   include_global_service_events = false
+#   is_multi_region_trail         = false
+#   enable_log_file_validation = true
 
-}
+# }
 
 resource "aws_s3_bucket" "example" {
   bucket = "example-bucket-${random_id.example.hex}"
